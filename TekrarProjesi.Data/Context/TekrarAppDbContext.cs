5bf5e604ead006f53f1c8d60e088e4f3a2da6eb3
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-10-31 18:19:30 +0300

    Add project files.

---
+++ TekrarProjesi.Data/Context/TekrarAppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TekrarProjesi.Data.Entities;
+
+namespace TekrarProjesi.Data.Context
+{
+    public class TekrarAppDbContext  : DbContext
+    {
+        public TekrarAppDbContext(DbContextOptions<TekrarAppDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new FeatureConfiguration());
+            modelBuilder.ApplyConfiguration(new HotelConfiguration());
+            modelBuilder.ApplyConfiguration(new HotelFeatureConfiguration());
+            modelBuilder.ApplyConfiguration(new ReservationConfiguration());
+            modelBuilder.ApplyConfiguration(new RoomConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+        public DbSet<FeatureEntity> Features => Set<FeatureEntity>();
+        public DbSet<HotelEntity> Hotels => Set<HotelEntity>();
+        public DbSet<HotelFeatureEntity> HotelFeatures => Set<HotelFeatureEntity>();
+        public DbSet<ReservationEntity> Reservations => Set<ReservationEntity>();
+        public DbSet<RoomEntity> Rooms => Set<RoomEntity>();
+        public DbSet<UserEntity> Users => Set<UserEntity>();
+
+    }
+}
---
+++ TekrarProjesi.Data/Repositories/Repository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TekrarProjesi.Data.Context;
+using TekrarProjesi.Data.Entities;
+
+namespace TekrarProjesi.Data.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+
+        private readonly TekrarAppDbContext _db;
+        private readonly DbSet<TEntity> _dbset;
+        public Repository(TekrarAppDbContext db)
+        {
+            _db = db;
+            _dbset = _db.Set<TEntity>();
+        }
+        public void Add(TEntity entity)
+        {
+            entity.CreatedDate = DateTime.Now;
+            _dbset.Add(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            entity.ModifiedDate = DateTime.Now;
+            entity.IsDeleted = true;
+            _dbset.Update(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var entity = _dbset.Find(id);
+            Delete(entity);
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbset.FirstOrDefault(predicate);
+        }
+
+        public IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            return predicate is null ? _dbset : _dbset.Where(predicate);
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _dbset.Find(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            entity.ModifiedDate = DateTime.Now;
+            _dbset.Update(entity);
+        }
+    }
+}
